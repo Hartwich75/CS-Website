d942090a86f07c8e2c7ac5aa492be7508990d35f
Mads Hartwich <madshartwich@gmail.com>
2024-08-01 02:48:20 +0200

    added function to hide ak images on click

--- main/JavaScript/weaponImageFunc.js
+++ main/JavaScript/weaponImageFunc.js
@@ -1,14 +1,18 @@
+function showSkinImages(id){
+    const skinImages = document.querySelector(".skinImages" + id);
+    if (skinImages) {
+        skinImages.style.display = "flex";
+    } 
+}
 
 function hideDefaultWeaponImages(){
     const defaultWeaponImages = document.querySelector(".defaultWeaponImages");
     defaultWeaponImages.style.display = "none";
 }
 
-function showSkinImages(id){
-    const skinImages = document.querySelector(".skinImages" + id);
-    if (skinImages) {
-        skinImages.style.display = "flex";
-    } 
+function hideSkinImagesAk(){
+    const skinImagesAk = document.querySelector(".skinImagesAk");
+    skinImagesAk.style.display = "none";
 }
 
 function hidepatternSkinImages(){
