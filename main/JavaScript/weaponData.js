05198696c75781f0c1a02119b29780f234883a8f
Mads Hartwich <madshartwich@gmail.com>
2024-08-03 19:53:48 +0200

    Added weapon ids and names for weapon data

--- main/JavaScript/weaponData.js
+++ main/JavaScript/weaponData.js
@@ -43,14 +43,169 @@ const weaponData = [
         img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_aug_png.png'
     },
     {
-        id: 'awp',
+        id: 'Awp',
         name: 'AWP',
         img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_awp_png.png'
     },
     {
-        id: 'cz-75',
-        name: 'cz-75',
+        id: 'Cz75',
+        name: 'CZ-75',
         img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_cz75a_png.png'
+    },
+    {
+        id: 'Deserteagle',
+        name: 'Deasert eagle',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Dualberettas',
+        name: 'Dual berettas',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Famas',
+        name: 'Famas',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Fiveseven',
+        name: 'Five-Seven',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'G3sg1',
+        name: 'G3SG1',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Galilar',
+        name: 'Galil AR',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Glock-18',
+        name: 'Glock-18',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'M249',
+        name: 'M249',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'M4a4',
+        name: 'M4A4',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'M4a1-s',
+        name: 'M4A1-S',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Mac-10',
+        name: 'Mac-10',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Mag-7',
+        name: 'Mag-7',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Mp5-sd',
+        name: 'MP5-SD',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Mp7',
+        name: 'MP7',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Mp9',
+        name: 'MP9',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Negev',
+        name: 'Negev',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Nova',
+        name: 'Nova',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Pp-bizon',
+        name: 'PP-Bizon',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'P2000',
+        name: 'P2000',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'P250',
+        name: 'P250',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'P90',
+        name: 'P90',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'R8revolver',
+        name: 'R8 Revolver',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Sawed-off',
+        name: 'Sawed-Off',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Scar-20',
+        name: 'SCAR-20',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Sg553',
+        name: 'SG553',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Ssg08',
+        name: 'SSG08',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Tec-9',
+        name: 'Tec-9',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Ump-45',
+        name: 'UMP-45',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Usp-s',
+        name: 'USP-S',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Xm1014',
+        name: 'XM1014',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
+    },
+    {
+        id: 'Zeus',
+        name: 'Zeus',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_deagle_png.png'
     },
 
 ];
