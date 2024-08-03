fd938393f58869ef3144859d562c4979125233e5
Mads Hartwich <madshartwich@gmail.com>
2024-08-03 19:33:50 +0200

    Functions to list and show weapon datas for each weapons and their skins

--- main/JavaScript/index.js
+++ main/JavaScript/index.js
@@ -6,6 +6,46 @@ document.addEventListener("DOMContentLoaded", function() {
         });
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const weaponContainer = document.querySelector(".defaultWeaponImages");
+    const weaponSkinsContainer = document.querySelector(".defaultWeaponImagesAk");
+
+    weaponData.forEach(weapon => {
+    
+        const weaponElement = document.createElement("div");
+        weaponElement.className = "weaponimage";
+        weaponElement.id = weapon.id;
+        weaponElement.innerHTML = `
+            <divclass="defaultWeaponImages" onclick="hideDefaultWeaponImages()">
+            <a onclick="showDefaultWeaponImages('${weapon.id}')">
+                <img src="${weapon.img}" width="270px" height="200px" alt="This is a picture of the ${weapon.name}" title="${weapon.name}">
+            </a>
+            <hr class="weaponimagehr">
+            <br>
+            <p>${weapon.name}</p>
+            </div>
+        `;
+        weaponContainer.appendChild(weaponElement);
+
+        if (weapon.skins) {
+            weapon.skins.forEach(skin => {
+                const skinElement = document.createElement("div");
+                skinElement.className = "weaponimage";
+                skinElement.id = skin.id;
+                skinElement.innerHTML = `
+                    <a onclick="showDefaultWeaponImages('${skin.id}')">
+                        <img src="${skin.img}" width="270px" height="200px" alt="This is a picture of the ${weapon.name} ${skin.name}" title="${weapon.name} ${skin.name}">
+                    </a>
+                    <hr class="weaponimagehr">
+                    <br>
+                    <p>${skin.name}</p>
+                `;
+                weaponSkinsContainer.appendChild(skinElement);
+            });
+        }
+    });
+});
+
 function showSidebar(){
 const sidebar = document.querySelector(".sidebar")
 sidebar.style.display = "flex"
---
+++ main/JavaScript/weaponData.js
@@ -0,0 +1,56 @@
+const weaponData = [
+    {
+        id: 'Ak',
+        name: 'AK-47',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_ak47_png.png',
+        skins: [
+            {
+                id: 'akCaseHardened',
+                name: 'Case Hardened',
+                img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/default_generated/weapon_ak47_aq_oiled_light_png.png'
+            },
+            {
+                id: 'akHeadShot',
+                name: 'Head Shot',
+                img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/default_generated/weapon_ak47_cu_ak_head_shot_holo_light_png.png'
+            },
+            {
+                id: 'akNightwish',
+                name: 'Nightwish',
+                img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/default_generated/weapon_ak47_cu_ak47_nightwish_light_png.png'
+            },
+            {
+                id: 'akLeetMuseo',
+                name: 'Leet Museo',
+                img: ''
+            },
+            {
+                id: 'akLegionOfAnubis',
+                name: 'Legion Of Anubis',
+                img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/default_generated/weapon_ak47_cu_ak47_anubis_light_png.png'
+            },
+            {
+                id: 'akAsiimov',
+                name: 'Asiimov',
+                img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/default_generated/weapon_ak47_cu_ak47_asiimov_light_png.png'
+            },
+            
+        ]
+    },
+    {
+        id: 'Aug',
+        name: 'AUG',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_aug_png.png'
+    },
+    {
+        id: 'awp',
+        name: 'AWP',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_awp_png.png'
+    },
+    {
+        id: 'cz-75',
+        name: 'cz-75',
+        img: 'https://raw.githubusercontent.com/mads369j/counter-strike-image-tracker/main/static/panorama/images/econ/weapons/base_weapons/weapon_cz75a_png.png'
+    },
+
+];
